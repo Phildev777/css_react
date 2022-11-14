1ff99fcf9beb43f985a1b292f5543f9bcca2e857
Philippe Berra <berraphilippe07@gmail.com>
2022-11-14 12:34:53 +0100

    quête Scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import style from 'styled-components';
+
+const Title = style.h1`
+font-family:'Rubik Bubbles', cursive;
+color:black;
+text-shadow: 0 -1px 4px #FFF, 0 -2px 10px #ff0, 0 -10px 20px #ff8000, 0 -18px 40px #F00;
+text-align:center;`;
+const Ul = style.div`
+display:flex;
+list-style-type:kannada;
+flex-direction:column;
+align-items:center;
+font-size:24px;
+`;
+const Document = style.div`
+-webkit-box-shadow: 4px -4px 15px 0px #FF1F1F, 12px -11px 7px 0px #FF9376, 20px -5px 7px 0px #FFE264, 20px 6px 7px 0px #F6FF33, 13px 12px 17px 0px #FF9527, 2px 17px 17px 0px #FF0000, -9px 21px 18px 0px #FFF212, -9px 6px 11px 0px #FF0808, -11px -9px 11px 0px #FFFA17, -11px -9px 11px 0px #FFFA17, -16px -14px 6px 5px rgba(0,0,0,0); 
+box-shadow: 4px -4px 15px 0px #FF1F1F, 12px -11px 7px 0px #FF9376, 20px -5px 7px 0px #FFE264, 20px 6px 7px 0px #F6FF33, 13px 12px 17px 0px #FF9527, 2px 17px 17px 0px #FF0000, -9px 21px 18px 0px #FFF212, -9px 6px 11px 0px #FF0808, -11px -9px 11px 0px #FFFA17, -11px -9px 11px 0px #FFFA17, -16px -14px 6px 5px rgba(0,0,0,0);
+margin-left:50px;
+margin-right:50px;
+background-image:url('https://cdn.pixabay.com/photo/2022/10/24/09/54/switzerland-7543063_960_720.jpg');`;
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Document>
+      <div>
+        <h1><Title>Projet Scss avec Style Components</Title></h1>
+        <div>
+          <Ul><Title>
+            <li>Débuts</li>
+            <li>Milieu</li>
+            <li>Fin</li>
+          </Title></Ul>
+          <Title><p>J'ai opté pour le "styles-components" de Scss car c'est celui que j'ai compris le plus rapidement et qui me parle le plus</p></Title>
+        </div>
+      </div>
+    </Document>
   );
 }
 
---
+++ src/components/Scss.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import './style.scss';
+
+
+
+const Scss = () => {
+
+    return (
+        <div>
+            <h1>ScSS</h1>
+        </div>
+    );
+};
+
+export default Scss;
